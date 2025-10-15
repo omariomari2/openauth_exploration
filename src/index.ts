284294328c909b0362fc750f086d1d3c49d81942
Bright Omari Owusu <188401332+omariomari2@users.noreply.github.com>
2025-10-15 16:44:51 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -65,13 +65,13 @@ export default {
 				}),
 			},
 			theme: {
-				title: "myAuth",
-				primary: "#0051c3",
-				favicon: "https://workers.cloudflare.com//favicon.ico",
+				title: "Go-Shop",
+				primary: "#FFF8DC",
+				favicon: "https://ik.imagekit.io/dr5fryhth/logo1.png?updatedAt=1760472240746",
 				logo: {
-					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+					dark: "https://ik.imagekit.io/dr5fryhth/logo1.png?updatedAt=1760472240746",
 					light:
-						"https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+						"https://ik.imagekit.io/dr5fryhth/logo1.png?updatedAt=1760472240746",
 				},
 			},
 			success: async (ctx, value) => {
